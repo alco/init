2c9b051405b80b7c4145b25383067a5cf6630b8e
Alexei Sholik <alcosholik@gmail.com>
2017-12-15 12:24:58 +0200

    Simplify startup config for fish

--- misc/fish/config.fish
+++ misc/fish/config.fish
@@ -1,13 +1,4 @@
 set -x EDITOR vim
 set -x LC_ALL en_US.UTF-8
 
-set PATH ~/Library/Python/2.7/bin/ $PATH
-
-set -x PATH ~/home/bin $PATH "$GOPATH/bin"
-
-set PATH ~/.rbenv/bin $PATH
-set PATH ~/.rbenv/shims $PATH
-rbenv rehash >/dev/null ^&1
-
-set -x IPFS_PATH ~/tmp/ipfs
 source ~/.asdf/asdf.fish
