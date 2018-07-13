cde9138116c1f0c811ad1cc0a1763001a8ab8671
Alexei Sholik <alcosholik@gmail.com>
2018-07-13 15:43:06 +0300

    Add OPAM config to config.fish

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -5,3 +5,11 @@ set -x COLORTERM truecolor
 set -x PATH $PATH /usr/local/sbin
 
 source /usr/local/opt/asdf/asdf.fish
+
+# OPAM configuration
+set -x PATH ~/.opam/system/bin $PATH
+set -x OCAML_TOPLEVEL_PATH ~/.opam/system/lib/toplevel
+set -x PERL5LIB "/Users/alco/.opam/system/lib/perl5:$PERL5LIB"
+set -x MANPATH $MANPATH ~/.opam/system/man
+set -x OPAMUTF8MSGS 1
+set -x CAML_LD_LIBRARY_PATH "/Users/alco/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs"
